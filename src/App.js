d27dafc5ccb55fe9f0adf132eb466c816e4c9528
Joseph Massabni <46639729+josephmassabni@users.noreply.github.com>
2020-07-13 10:59:40 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/layout/header';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import About from './components/pages/About'
+import { v4 as uuid } from 'uuid';
+
+import './App.css';
+
+
+class App extends React.Component {
+  state = {
+  todos: [//Array of todos that will be added to
+         ]
+  }
+
+  //Toggle Complete property of Todo
+  markComplete = (id) => {
+    this.setState({ todos: this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    }) });
+  }
+
+  //Delete Todo
+  delTodo = (id) => {
+    this.setState({ todos: [...this.state.todos.filter(todo => todo.id !== id)] })
+  }
+
+  //Add Todo
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid(),
+      title,
+      completed: false
+    }
+    this.setState({todos: [...this.state.todos, newTodo]});
+  } 
+
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Header />
+          <Route exact path="/" render={props => (
+            <React.Fragment>
+              <AddTodo addTodo={this.addTodo}/>
+              <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo} />
+            </React.Fragment>
+          )} />
+          <Route path="/about" component={About} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <br></br>
+            <p>This is a To Do List App made with React and Node.js</p>
+        </React.Fragment>
+    )
+}
+
+export default About;
